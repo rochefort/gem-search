bbea30fc1305eefbac5b4eaad53532424d0ac980
rochefort <terasawan@gmail.com>
2014-05-10 21:26:56 +0900

    Remove unnecessary dap method

--- lib/gem-search/rendering.rb
+++ lib/gem-search/rendering.rb
@@ -21,14 +21,14 @@ module Gem::Search
       end
 
       def render_to_header
-        f=DEFAULT_RULED_LINE_SIZE.dup
+        f=DEFAULT_RULED_LINE_SIZE
         fmt = "%-#{f[0]}s %#{f[1]}s %#{f[2]}s"
         puts fmt % ['NAME', 'DL(ver)', 'DL(all)']
         puts fmt % ['-'*f[0], '-'*f[1], '-'*f[2]]
       end
 
       def render_to_body(gems)
-        f=DEFAULT_RULED_LINE_SIZE.dup
+        f=DEFAULT_RULED_LINE_SIZE
         fmt = "%-#{f[0]}s %#{f[1]}d %#{f[2]}d"
         gems.each do |gem|
           puts fmt % [
