9df66b7fd78bff7467961e2bdb34b4b2135c6a87
rochefort <terasawan@gmail.com>
2014-05-10 18:02:31 +0900

    cosme [ci skip]

--- lib/gem-search.rb
+++ lib/gem-search.rb
@@ -1,6 +1,6 @@
 module Gem
   module Search
-    autoload :Executor   , 'gem-search/executor'
+    autoload :Executor , 'gem-search/executor'
     autoload :Rendering, 'gem-search/rendering'
     autoload :VERSION  , 'gem-search/version'
   end
