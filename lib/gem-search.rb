74ca3a5187b73f3d1ecae959ddb74cacb124d5b4
rochefort <terasawan@gmail.com>
2014-05-10 20:22:03 +0900

    Extract slop logic to Command class

--- lib/gem-search.rb
+++ lib/gem-search.rb
@@ -1,5 +1,6 @@
 module Gem
   module Search
+    autoload :Command ,  'gem-search/command'
     autoload :Executor , 'gem-search/executor'
     autoload :Rendering, 'gem-search/rendering'
     autoload :VERSION  , 'gem-search/version'
---
+++ lib/gem-search/command.rb
@@ -0,0 +1,54 @@
+require 'slop'
+
+class Slop
+  def opt_description(msgs)
+    ind = ' ' * 22
+    msgs.inject { |rtn, msg| rtn << "\n#{ind}#{msg}" }
+  end
+end
+
+module Gem::Search
+  class Command
+    ENABLE_SORT_OPT = {
+      'v' => 'version_downloads',
+      'a' => 'downloads',
+    }
+
+    OPTS = Slop.parse(help: true) do
+      banner "Usage: gem-search gem_name [options]\n"
+      on :s, :sort, opt_description([
+          'Sort by the item.',
+          '  [n]ame :default  eg. gem-search webkit',
+          '  [v]er  :DL(ver)  eg. gem-search webkit -s v',
+          '  [a]ll  :DL(all)  eg. gem-search webkit -s a'
+        ]), :argument => :optional
+      on :v, :version, 'Display the version.'
+    end
+
+    def run
+      version if OPTS['v']
+      validate
+
+      gs = Gem::Search::Executor.new
+      gs.search(ARGV[0], ENABLE_SORT_OPT[OPTS['sort']])
+    end
+
+    private
+      def version
+        puts "gem-search #{Gem::Search::VERSION}"
+        exit
+      end
+
+      def validate
+        if (ARGV.size == 0)
+          puts OPTS
+          abort
+        end
+
+        if ARGV.any? { |arg| arg.match(/\A-/) }
+          puts OPTS
+          exit
+        end
+      end
+  end
+end
