80bb88164ea28a8caba7067f34a80dcf7a4cf822
rochefort <terasawan@gmail.com>
2014-08-14 05:37:20 +0900

    Correspond to linux with browse option

--- lib/gem_search/executor.rb
+++ lib/gem_search/executor.rb
@@ -42,8 +42,17 @@ module Gem::Search
 
     private
 
+    # https://github.com/github/hub/blob/9c589396ae38f7b9f98319065ad491149954c152/lib/hub/context.rb#L517
     def browser_open(url)
-      system('open', url)
+      cmd = osx? ? 'open' :
+        %w[xdg-open cygstart x-www-browser firefox opera mozilla netscape].find { |comm| which comm }
+      system(cmd, url)
+    end
+
+    # refer to: https://github.com/github/hub/blob/9c589396ae38f7b9f98319065ad491149954c152/lib/hub/context.rb#L527
+    def osx?
+      require 'rbconfig'
+      RbConfig::CONFIG['host_os'].to_s.include?('darwin')
     end
 
     def open_rubygems_api(url)
