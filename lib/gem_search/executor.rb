e4f3eb7b4ae22e3a3cdd1f831ea787a83923220a
rochefort <terasawan@gmail.com>
2014-08-14 06:01:14 +0900

    Fix failure travis CI#43
    undefined method `which'

--- lib/gem_search/executor.rb
+++ lib/gem_search/executor.rb
@@ -55,6 +55,17 @@ module Gem::Search
       RbConfig::CONFIG['host_os'].to_s.include?('darwin')
     end
 
+    def which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = "#{path}/#{cmd}#{ext}"
+          return exe if File.executable? exe
+        }
+      end
+      return nil
+    end
+
     def open_rubygems_api(url)
       option = {}
       proxy = URI.parse(url).find_proxy
