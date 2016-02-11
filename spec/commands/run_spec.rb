47b6f8ecd927dabc0675ffb8a670ea2efd32d7a7
rochefort <terasawan@gmail.com>
2016-02-12 04:29:56 +0900

    Update rspec3 strict style

--- spec/commands/run_spec.rb
+++ spec/commands/run_spec.rb
@@ -5,31 +5,27 @@ include Gem::Search
 RSpec.describe Commands::Run do
   describe '#call' do
     before do
-      @executor = Executor.new
-      allow(Executor).to receive(:new).and_return(@executor)
-      @options = {}
-      allow(@options).to receive(:arguments).and_return([query])
-      allow(@options).to receive(:detail?).and_return(false)
+      allow(options).to receive(:arguments).and_return([query])
+      allow(options).to receive(:detail?).and_return(false)
+      allow(options).to receive(:[]).with('sort')
       stub_request_search(query, 1, dummy_search_result)
       stub_request_search_no_result_with_page(query, 2)
     end
-
     let(:query) { 'factory_girl' }
+    let(:options) { double('options') }
 
     context 'without sort option' do
       it 'called Executor#search without sort option' do
-        expect(@executor).to receive(:search).with(query, default_opts).once
-        Commands::Run.new(@options).call
+        expect_any_instance_of(Executor).to receive(:search).with(query, default_opts).once
+        Commands::Run.new(options).call
       end
     end
 
     context 'with sort option' do
-      before do
-        allow(@options).to receive(:sort).and_return('a')
-      end
+      before { allow(@options).to receive(:sort).and_return('a') }
       it 'called Executor#search with sort option' do
-        expect(@executor).to receive(:search).with(query, default_opts(sort: 'downloads')).once
-        Commands::Run.new(@options).call
+        expect_any_instance_of(Executor).to receive(:search).with(query, default_opts(sort: 'downloads')).once
+        Commands::Run.new(options).call
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,21 @@ require 'webmock/rspec'
 require 'gem_search'
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
   config.expose_dsl_globally = false
+
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+  config.order = :random
+  Kernel.srand config.seed
 end
 
 def load_http_stubs(file_name)
