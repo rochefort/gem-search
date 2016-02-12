c57b02ea3300731e96ea2b24a7c03ed7350e076d
rochefort <terasawan@gmail.com>
2016-02-13 03:25:05 +0900

    Remove rubocop warnings

--- lib/gem_search/commands/browse.rb
+++ lib/gem_search/commands/browse.rb
@@ -4,13 +4,13 @@ module Gem::Search
       def call
         executor = Executor.new
         executor.browse(options[:browse])
-        rescue OpenURI::HTTPError
-          puts 'No such gem.'
-          abort
-        rescue => e
-          puts e.message
-          puts e.stacktrace if ENV['DEBUG']
-          abort
+      rescue OpenURI::HTTPError
+        puts 'No such gem.'
+        abort
+      rescue => e
+        puts e.message
+        puts e.stacktrace if ENV['DEBUG']
+        abort
       end
     end
   end
